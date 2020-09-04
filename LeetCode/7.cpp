62bf41b3e2c03cf3c65bc84a58978c0ecb290d04
Артем Полтавський <54792047+artemexistent@users.noreply.github.com>
2020-09-04 14:48:37 +0300

    Create 7.cpp

---
+++ LeetCode/7.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int ans=0;
+        while (x!=0){
+            if (ans > INT_MAX/10 || (ans == INT_MAX / 10 && x%10 > 7)) return 0;
+            if (ans < INT_MIN/10 || (ans == INT_MIN / 10 && x%10 < -8)) return 0;
+            int t=ans*10+x%10;
+            ans=t;
+            x/=10;
+        }
+      
+        return ans;
+    }
+};
